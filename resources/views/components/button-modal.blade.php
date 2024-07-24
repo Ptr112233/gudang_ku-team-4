d917017d7721aa2777015829df1789f416467239
Jazilsyah13708 <mrjhen514@gmail.com>
2024-07-24 18:41:40 +0900

    Delete resources directory

--- resources/views/components/button-modal.blade.php
+++
@@ -1,5 +0,0 @@
-<a href="#" {{ $attributes->merge(['class' => '']) }} data-toggle="modal"
-    data-target="#modal-simple{{ $id }}">
-    <i class="fas fa-{{ $icon }} {{ $style }}"></i>
-    {{ $title }}
-</a>
